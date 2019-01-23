7d56a21b7ba89c8c96e5bf6b279cb20ffb2ede41
Angel Virgilio Cueto González <AngelCueto@MacBook-Pro-de-Samuel-2.local>
2019-01-23 01:15:27 -0600

    createUser template with no links or icons

--- MaterialDesignProject/scripts/app.js
+++ MaterialDesignProject/scripts/app.js
@@ -6,3 +6,6 @@ const ripple = new MDCRipple(document.querySelector('.foo-button'));
 console.log('hello world');
 
 const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
+
+const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
+iconButtonRipple.unbounded = true;
