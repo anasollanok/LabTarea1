9159279b87049e84a4d5410021ddc0d3b1e744ed
Bruno Schalch <brunoschalch@gmail.com>
2019-01-22 18:19:10 -0600

    Material textfield

--- MaterialDesignProject/scripts/app.js
+++ MaterialDesignProject/scripts/app.js
@@ -1,4 +1,8 @@
 import {MDCRipple} from '@material/ripple/index';
+import {MDCTextField} from '@material/textfield';
+
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
 
 console.log('hello world');
+
+const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
