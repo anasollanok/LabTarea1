bfeb2e5fb19385c94f8b127f2f89a31853211683
Bruno Schalch <brunoschalch@gmail.com>
2019-02-06 00:39:01 -0600

    html tests completed

--- test/specs/loginTest.js
+++ test/specs/loginTest.js
@@ -2,66 +2,67 @@ var assert = require("assert");
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
 describe('HTML tests', () => {
-    it('Check that element exists', ()=>{
+    it('Check that username input element exists', ()=>{
       browser.url('/');
-/*
-      var number = driver.findElements( By.id("username-input") ).size();
-      assert.equal(number, 0);
-      */
-     var user= $('#username-input');
-     var use2r= $('#username2-input');
-     console.log(user);
-     //var element = driver.findElements("id", "username-input" );
-     //var element = driver.findElement(By.id("username-input"));
-
-     // Revisa si existe el elemento.
-     assert.notEqual(user.ELEMENT , null);
-    // assert.notEqual(use2r.ELEMENT , null);
-     /*
-      var element = document.getElementById("username-input");
-      assert.notEqual(element , null);
-      */
-  
+      var user= $('#username-input');
+      assert.notEqual(user.ELEMENT , null);
     });
-  });
-/*
-WebElement Image = driver.findElement(By.xpath("//img[@border='0']"));
+
+    it('Should do nothing when no data in any text field', ()=>{
+        browser.url('/');
+        const button = $('#submitButton');
+        button.click();
+        var titulo = browser.getTitle();
+        assert.equal(titulo, "Login");
+      });
+
+      it('Should do nothing when no data in username text field', ()=>{
+        browser.url('/');
         
-//Get width of element.
-int ImageWidth = Image.getSize().getWidth();
-System.out.println("Image width Is "+ImageWidth+" pixels");
+        var pass = $('#password-input');
+        pass.setValue('passwordcomplete2');
 
-//Get height of element.
-int ImageHeight = Image.getSize().getHeight();        
-System.out.println("Image height Is "+ImageHeight+" pixels");
-*/
-/*
-describe('Login page', () => {
-    it('encontrar el titulo de la pagina', ()=>{
-      browser.url('/');
-      var titulo = browser.getTitle();
-      assert.equal(titulo, "Login Page");
-  
-    });
+        const button = $('#submitButton');
+        button.click();
+        var titulo = browser.getTitle();
+        assert.equal(titulo, "Login");
+      }); 
+
+      it('Should do nothing when no data in password text field', ()=>{
+        browser.url('/');
+        
+        var user= $('#username-input');
+        user.setValue('juan@tec.mx');
+
+        const button = $('#submitButton');
+        button.click();
+        var titulo = browser.getTitle();
+        assert.equal(titulo, "Login");
+      }); 
+
+      it('Should go somewhere when there is data in both text fields', ()=>{
+        browser.url('/');
+        
+        var user= $('#username-input');
+        user.setValue('juan@tec.mx');
+        var pass = $('#password-input');
+        pass.setValue('passwordcomplete2');
+
+        const button = $('#submitButton');
+        button.click();
+        var titulo = browser.getTitle();
+        assert.notEqual(titulo, "Login");
+      }); 
+
+      it('Check button size', ()=>{
+        browser.url('/');
+
+        const button = $('#submitButton');
+        var btnSize = button.getSize();
+        console.log("image size isss"+btnSize);
+        assert(btnSize.width > btnSize.height);
+      });
   });
-
-
-  describe('Login page 2', () => {
-    it('encontrar el titulo de la pagina', ()=>{
-      browser.url('/createUser.html?username=dbfsdf&password=sdfsdfdsf');
-      var titulo = browser.getTitle();
-      browser.getTitle().then(function(title){
-        expect(title).equals( "Login Page");
-    });
-    /*
-      if (assert.equal(titulo, "Login Page")) {
-          console.log("test successful");
-      } else {
-        console.log("test failed");
-      }
-      */
-    
-  /*
-    });
+  describe('CSS tests', () => {
+      
   });
-*/
