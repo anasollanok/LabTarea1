ea9fd8e87742e00613c0472f8f0b9c0d3ab1c745
anasollano <anasollanokim@gmail.com>
2019-02-20 23:37:32 -0600

    Done with CSS and HTML
    Now testing!

--- scripts/app.js
+++ scripts/app.js
@@ -2,31 +2,16 @@ import {MDCRipple} from '@material/ripple';
 import {MDCTextField} from '@material/textfield';
 import {MDCTopAppBar} from '@material/top-app-bar/index';
 import {MDCFormField} from '@material/form-field';
-import {MDCCheckbox} from '@material/checkbox';
 import {MDCChipSet} from '@material/chips';
 
-const ripple = new MDCRipple(document.querySelector('.foo-button'));
-
-const username = new MDCTextField(document.querySelector('.username'));
-const password = new MDCTextField(document.querySelector('.password'));
-
-if(document.querySelector('.phone')){
-  const phone = new MDCTextField(document.querySelector('.phone'));
-  const email = new MDCTextField(document.querySelector('.email'));
-  const address = new MDCTextField(document.querySelector('.address'));
-  const city = new MDCTextField(document.querySelector('.city'));
-  const state = new MDCTextField(document.querySelector('.state'));
+const serial = new MDCTextField(document.querySelector('.serial'));
+if(document.querySelector('.modulo3')){
+  const msg = new MDCTextField(document.querySelector('.msg'));
+  const mail = new MDCTextField(document.querySelector('.mail'));
+  const send = new MDCRipple(document.querySelector('.send'));
 }
-else{
-  const checkbox = new MDCCheckbox(document.querySelector('.mdc-checkbox'));
-  const formField = new MDCFormField(document.querySelector('.mdc-form-field'));
-  formField.input = checkbox;
-}
-const iconButtonRipple = new MDCRipple(document.querySelector('.mdc-icon-button'));
-iconButtonRipple.unbounded = true;
-const topAppBarElement = document.querySelector('.mdc-top-app-bar');
-const topAppBar = new MDCTopAppBar(topAppBarElement);
-const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
-
+const verify = new MDCRipple(document.querySelector('.verify'));
 const chipSetEl = document.querySelector('.mdc-chip-set');
 const chipSet = new MDCChipSet(chipSetEl);
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
